e9025ebc954c3c393bb2ad931a5d6d8ed6b51f8c
Narmatsu <joncyy_19@hotmail.com>
2023-11-08 12:23:09 -0500

    improvements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
-import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { Switch, Route } from "react-router-dom";
 
 const App = () => (
   <div className="App">
