806e6a0e39bbe0d0379eaf62369085feaf3c1dd8
Narmatsu <joncyy_19@hotmail.com>
2023-11-10 11:17:44 -0500

    fixed Slider component

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -4,7 +4,7 @@ import Container from "../Container";
 import { BsArrowLeftShort, BsArrowRightShort, BsGeoAlt } from "react-icons/bs";
 import "./Slider.style.scss";
 
-const data = Array(5).fill({
+const data = Array(6).fill({
   img: "https://www.civitatis.com/f/colombia/cartagena-de-indias/excursion-playa-blanca-589x392.jpg",
   info: {
     name: "Playa blanca",
@@ -14,20 +14,23 @@ const data = Array(5).fill({
 
 const Slider = ({ width = "70%" }) => {
   const refSlider = useRef(null);
+  const percentaje = parseFloat(width) / 100;
   const [sliderLeft, setSliderLeft] = useState(0);
   const [sliderLeftPixel, setSliderLeftPixel] = useState("0px");
-  const [actualWidth, setActualWidth] = useState(window.innerWidth);
+  const [actualWidth, setActualWidth] = useState(
+    window.innerWidth * percentaje
+  );
   const itemsWidth = width ? width : "";
 
   useEffect(() => {
     const handlerWindowSize = () => {
-      setActualWidth(window.innerWidth);
+      setActualWidth(window.innerWidth * percentaje);
     };
     window.addEventListener("resize", handlerWindowSize);
     return () => {
       window.removeEventListener("resize", handlerWindowSize);
     };
-  }, []);
+  }, [percentaje]);
 
   useEffect(() => {
     setSliderLeftPixel(`${sliderLeft * 270}px`);
@@ -38,25 +41,17 @@ const Slider = ({ width = "70%" }) => {
   };
 
   const handlerShowPrevious = () => {
-    setSliderLeft(sliderLeft - 1);
+    if (rightActive()) setSliderLeft(sliderLeft - 1);
   };
 
-  // todo: improve
-  const showControls = () => {
-    if (actualWidth <= 1899 && data.length >= 5) return true;
-    if (actualWidth <= 1500 && data.length >= 4) return true;
-    if (actualWidth <= 1140 && data.length >= 3) return true;
-    if (actualWidth <= 350 && data.length >= 1) return true;
-    return false;
-  };
+  const showControls = () => (actualWidth >= data.length * 270 ? false : true);
 
   const leftActive = () => (sliderLeft < 0 ? "active" : "");
 
-  // todo: fix
-  const rightActive = () =>
-    (data.length - sliderLeft) * 270 < refSlider?.current?.offsetWidth || 0
-      ? "active"
-      : "";
+  const rightActive = () => {
+    const panelWidth = refSlider?.current?.offsetWidth || actualWidth;
+    return (data.length + sliderLeft) * 270 > panelWidth ? "active" : "";
+  };
 
   return (
     <section className="app__slider">
