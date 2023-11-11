aba5115ef0033ff61afd8a49dadb2e7010761d9b
Narmatsu <joncyy_19@hotmail.com>
2023-11-11 18:52:04 -0500

    improve CardPresentation component

--- src/components/CardPresentation/CardPresentation.jsx
+++ src/components/CardPresentation/CardPresentation.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-import { BsGeoAlt } from "react-icons/bs";
+import { BsFillStarFill, BsGeoAlt } from "react-icons/bs";
 import "./CardPresentation.style.scss";
 
-const CardPresentation = ({ img, name, location }) => {
-  return (
-    <section className="card__presentation">
-      <img src={img} alt={name} />
-      <section className="brightness" />
-      <section className="card__presentation__caption">
-        <b>{name}</b>
-        <p>
-          <BsGeoAlt />
-          {location}
-        </p>
+const CardPresentation = ({ img, name, location, price, rating }) => (
+  <section className="card__presentation">
+    {rating && (
+      <section className="card__presentation__rating">
+        <BsFillStarFill />
+        {rating}
       </section>
+    )}
+    <img src={img} alt={name} />
+    <section className="brightness" />
+    <section className="card__presentation__caption">
+      <b>{name}</b>
+      <p>
+        <BsGeoAlt />
+        {location}
+      </p>
+      {price && (
+        <label className="card__presentation__caption__price">${price}</label>
+      )}
     </section>
-  );
-};
+  </section>
+);
 
 export default CardPresentation;
