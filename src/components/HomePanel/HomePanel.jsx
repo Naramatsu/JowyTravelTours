b2c634c551d0b09a14bc523566484ade7d47d8c0
Narmatsu <joncyy_19@hotmail.com>
2023-11-15 16:59:18 -0500

    improvements

--- src/components/HomePanel/HomePanel.jsx
+++ src/components/HomePanel/HomePanel.jsx
@@ -1,38 +1,34 @@
-import React, { useContext } from "react";
+import React from "react";
 import Grid from "../../layout/Grid";
 import TableHeader from "../TableHeader";
 import { btnSearch, hospedajeForm } from "./data";
-import { PreferencesAppContext } from "../../context/Preferences";
 import "./HomePanel.style.scss";
 
 const classNamePrefixComponent = "home__panel";
 
-const HomePanel = () => {
-  const { languaje } = useContext(PreferencesAppContext);
-  return (
-    <section className={`${classNamePrefixComponent}`}>
-      <TableHeader />
-      <section className={`${classNamePrefixComponent}__container`}>
-        <Grid columns={2}>
-          {hospedajeForm[languaje].map(({ label, name, icon }, index) => (
-            <section className="input__control" key={index}>
-              <label>{label}</label>
-              <section className="input__control__field">
-                <input name={name} placeholder={label} />
-                {icon}
-              </section>
+const HomePanel = ({ languaje }) => (
+  <section className={`${classNamePrefixComponent}`}>
+    <TableHeader />
+    <section className={`${classNamePrefixComponent}__container`}>
+      <Grid columns={2}>
+        {hospedajeForm[languaje].map(({ label, name, icon }, index) => (
+          <section className="input__control" key={index}>
+            <label>{label}</label>
+            <section className="input__control__field">
+              <input name={name} placeholder={label} />
+              {icon}
             </section>
-          ))}
-        </Grid>
-        <section className={`${classNamePrefixComponent}__button`}>
-          <button>
-            {btnSearch.icon}
-            {btnSearch[languaje].label}
-          </button>
-        </section>
+          </section>
+        ))}
+      </Grid>
+      <section className={`${classNamePrefixComponent}__button`}>
+        <button>
+          {btnSearch.icon}
+          {btnSearch[languaje].label}
+        </button>
       </section>
     </section>
-  );
-};
+  </section>
+);
 
 export default HomePanel;
