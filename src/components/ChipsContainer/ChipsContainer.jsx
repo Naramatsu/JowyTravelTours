4ba855868ca9a391e2acc5857dae42d0f8a3283f
Narmatsu <joncyy_19@hotmail.com>
2023-12-06 18:35:59 -0500

    added chips sublist for sub categories

--- src/components/ChipsContainer/ChipsContainer.jsx
+++ src/components/ChipsContainer/ChipsContainer.jsx
@@ -1,14 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { BiAward } from "react-icons/bi";
 import { isActiveTab } from "../../utils";
+import { IoMdCloseCircle } from "react-icons/io";
 import { PreferencesAppContext } from "../../context/Preferences";
 import "./ChipsContainer.style.scss";
 
-const ChipsContainer = ({ chips, chipActive, onSelectChip }) => {
+const ChipsContainer = ({
+  chips,
+  chipActive,
+  onSelectChip,
+  subChips,
+  subChipActive,
+  onSelectSubChip,
+}) => {
   const { theme } = useContext(PreferencesAppContext);
+  const [subProductList, setSubProductList] = useState([]);
+
+  useEffect(() => {
+    if (chipActive) setSubProductList(subChips[chipActive]);
+  }, [chips, subChips, chipActive]);
+
+  const isVisibleMainList = chipActive ? "visible" : "hidden";
 
   return (
-    <section className={`chip__container ${theme}`}>
+    <section className={`chip__container ${theme} ${isVisibleMainList}`}>
       {chips.map((item, index) => (
         <section
           key={index}
@@ -19,6 +34,21 @@ const ChipsContainer = ({ chips, chipActive, onSelectChip }) => {
           {item}
         </section>
       ))}
+      <section className="chip__sublist">
+        {subProductList.map((subCip, index) => (
+          <section
+            key={index}
+            className={`app__chip ${isActiveTab(subCip, subChipActive)}`}
+            onClick={() => onSelectSubChip(subCip)}
+          >
+            <BiAward />
+            {subCip}
+          </section>
+        ))}
+        <section className={"app__chip close"} onClick={() => onSelectChip("")}>
+          <IoMdCloseCircle />
+        </section>
+      </section>
     </section>
   );
 };
