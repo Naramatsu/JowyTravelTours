184a2406021b720d70b0cd1b09ecc82dcd2530c5
Narmatsu <joncyy_19@hotmail.com>
2023-12-18 12:21:33 -0500

    added AboutUs Page

---
+++ src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,86 @@
+import React, { useContext } from "react";
+import Container from "../../layout/Container";
+import { PreferencesAppContext } from "../../context/Preferences";
+import {
+  howItWorks,
+  outstandingInfo,
+  pageTitle,
+  panelDescription,
+  panelTitle,
+  steps,
+  whyChooseUs,
+  whyPanelTitle,
+} from "./data";
+import PanelImage from "../../components/PanelImage";
+import Shape from "../../components/Shape";
+import Arrow from "../../assets/arrow.png";
+import "./AboutUs.style.scss";
+import { imgBanner } from "../../utils/constants";
+import BannerPage from "../../components/BannerPage/BannerPage";
+
+const AboutUs = () => {
+  const { theme, languaje } = useContext(PreferencesAppContext);
+  return (
+    <section className="aboutus">
+      <BannerPage title={pageTitle[languaje]} img={imgBanner} />
+      <Container width="70%" className="aboutus__panel">
+        <PanelImage
+          img={imgBanner}
+          theme={theme}
+          title={panelTitle[languaje]}
+          className="aboutus__panel__container"
+          description={panelDescription[languaje]}
+        >
+          <section className="shape__container__list">
+            {outstandingInfo[languaje].map(({ cuantity, label }, index) => (
+              <Shape key={index} cuantity={cuantity} label={label} />
+            ))}
+          </section>
+        </PanelImage>
+        <section className="how-it-works">
+          <section className="how-it-works__title">
+            <h3>{howItWorks[languaje].title}</h3>
+            {howItWorks[languaje].description}
+          </section>
+          <section className="how-it-works__steps">
+            {steps[languaje].map(({ index, label, description }) => (
+              <React.Fragment key={index}>
+                <section className="whyUs__panel">
+                  <Shape cuantity={index} />
+                  <section className="whyUs__panel__caption">
+                    <h4>{label}</h4>
+                    {description}
+                  </section>
+                </section>
+                <img className={`arrow ${theme}`} src={Arrow} alt="arrow" />
+              </React.Fragment>
+            ))}
+          </section>
+        </section>
+        <PanelImage
+          img={imgBanner}
+          theme={theme}
+          title={whyPanelTitle[languaje]}
+          className="whyUs__panel__container"
+          side="right"
+        >
+          <section className="whyUs__container__list">
+            {whyChooseUs[languaje].map(
+              ({ cuantity, label, description }, index) => (
+                <section key={index} className={`whyUs__panel ${theme}`}>
+                  <Shape cuantity={cuantity} />
+                  <section className="whyUs__panel__caption">
+                    <h4>{label}</h4>
+                    {description}
+                  </section>
+                </section>
+              )
+            )}
+          </section>
+        </PanelImage>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutUs;
---
+++ src/pages/AboutUs/data.js
@@ -0,0 +1,217 @@
+import { ENGLISH, SPANISH } from "../../utils/constants";
+
+export const pageTitle = {
+  [SPANISH]: "Sobre Nosotros",
+  [ENGLISH]: "About Us",
+};
+
+export const panelTitle = {
+  [SPANISH]: (
+    <h2>
+      Sobre <br /> Jowy Travel & Tours
+    </h2>
+  ),
+  [ENGLISH]: (
+    <h2>
+      About <br /> Jowy Travel & Tours
+    </h2>
+  ),
+};
+
+export const panelDescription = {
+  [SPANISH]: (
+    <>
+      <p>
+        Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+        archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de
+        las industrias desde el año 1500, cuando un impresor (N. del T. persona
+        que se dedica a la imprenta)
+      </p>
+      <p>
+        desconocido usó una galería de textos y los mezcló de tal manera que
+        logró hacer un libro de textos especimen. No sólo sobrevivió 500 años
+      </p>
+    </>
+  ),
+  [ENGLISH]: (
+    <>
+      <p>
+        Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+        archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de
+        las industrias desde el año 1500, cuando un impresor (N. del T. persona
+        que se dedica a la imprenta)
+      </p>
+      <p>
+        desconocido usó una galería de textos y los mezcló de tal manera que
+        logró hacer un libro de textos especimen. No sólo sobrevivió 500 años
+      </p>
+    </>
+  ),
+};
+
+export const outstandingInfo = {
+  [SPANISH]: [
+    { cuantity: "10", label: "Años Exp" },
+    { cuantity: "10+", label: "Hoteles" },
+    { cuantity: "3", label: "Ciudades disponibles" },
+  ],
+  [ENGLISH]: [
+    { cuantity: "10", label: "Years Exp" },
+    { cuantity: "10+", label: "Hotels" },
+    { cuantity: "3", label: "Cities availables" },
+  ],
+};
+
+export const whyPanelTitle = {
+  [SPANISH]: <h2>¿Por qué Elegirnos?</h2>,
+  [ENGLISH]: (
+    <h2>
+      Why <br /> choose US?
+    </h2>
+  ),
+};
+
+export const whyChooseUs = {
+  [SPANISH]: [
+    {
+      cuantity: "01",
+      label: "Nos ajustamos a tu presupuesto",
+      description: (
+        <p>
+          desconocido usó una galería de textos y los mezcló de tal manera que
+          logró hacer un libro de textos especimen
+        </p>
+      ),
+    },
+    {
+      cuantity: "02",
+      label: "Somos confiables",
+      description: (
+        <p>
+          desconocido usó una galería de textos y los mezcló de tal manera que
+          logró hacer un libro de textos especimen
+        </p>
+      ),
+    },
+    {
+      cuantity: "03",
+      label: "Nos importas demasiado",
+      description: (
+        <p>
+          desconocido usó una galería de textos y los mezcló de tal manera que
+          logró hacer un libro de textos especimen
+        </p>
+      ),
+    },
+    {
+      cuantity: "04",
+      label: "Nos importas demasiado",
+      description: (
+        <p>
+          desconocido usó una galería de textos y los mezcló de tal manera que
+          logró hacer un libro de textos especimen
+        </p>
+      ),
+    },
+  ],
+  [ENGLISH]: [
+    {
+      cuantity: "01",
+      label: "We fit your budget",
+      description: (
+        <p>
+          desconocido usó una galería de textos y los mezcló de tal manera que
+          logró hacer un libro de textos especimen
+        </p>
+      ),
+    },
+    {
+      cuantity: "02",
+      label: "We are reliable",
+      description: (
+        <p>
+          desconocido usó una galería de textos y los mezcló de tal manera que
+          logró hacer un libro de textos especimen
+        </p>
+      ),
+    },
+    {
+      cuantity: "03",
+      label: "We care too much about you",
+      description: (
+        <p>
+          desconocido usó una galería de textos y los mezcló de tal manera que
+          logró hacer un libro de textos especimen
+        </p>
+      ),
+    },
+    {
+      cuantity: "04",
+      label: "We care too much about you",
+      description: (
+        <p>
+          desconocido usó una galería de textos y los mezcló de tal manera que
+          logró hacer un libro de textos especimen
+        </p>
+      ),
+    },
+  ],
+};
+
+export const steps = {
+  [SPANISH]: [
+    {
+      index: 1,
+      label: "Me Contactas",
+      description: <p>Te damos asesoria</p>,
+    },
+    {
+      index: 2,
+      label: "Nos amoldamos a tu presupuesto",
+      description: <p>Buscamos de acuerdo a tus necesidades</p>,
+    },
+    {
+      index: 3,
+      label: "Generamos la reserva",
+      description: <p>Para que disfrutes de tus vacaciones</p>,
+    },
+  ],
+  [ENGLISH]: [
+    {
+      index: 1,
+      label: "Contact me",
+      description: <p>We give you advice</p>,
+    },
+    {
+      index: 2,
+      label: "We adapt to your budget",
+      description: <p>We search according to your needs</p>,
+    },
+    {
+      index: 3,
+      label: "We generate the reservation",
+      description: <p>We search according to your needs</p>,
+    },
+  ],
+};
+
+export const howItWorks = {
+  [SPANISH]: {
+    title: "¿Cómo funciona?",
+    description: (
+      <p>
+        desconocido usó una galería de textos y los mezcló de tal manera que
+        logró hacer un libro de textos especimen
+      </p>
+    ),
+  },
+  [ENGLISH]: {
+    title: "How it Works",
+    description: (
+      <p>
+        desconocido usó una galería de textos y los mezcló de tal manera que
+        logró hacer un libro de textos especimen
+      </p>
+    ),
+  },
+};
