11ab2c772eb5926b19442f6e63023dbe4916238e
Narmatsu <joncyy_19@hotmail.com>
2024-03-05 16:01:36 -0500

    Video Promo component was updated

---
+++ src/components/VideoPromo/VideoPromo.data.js
@@ -0,0 +1,53 @@
+import { FiUsers } from "react-icons/fi";
+import { ENGLISH, SPANISH } from "../../utils/constants";
+import { AiOutlineStar } from "react-icons/ai";
+
+export const cartagenaInfo = {
+  [SPANISH]: [
+    {
+      icon: <FiUsers />,
+      label: "35.000",
+      name: "Visitantes",
+    },
+    {
+      icon: <AiOutlineStar />,
+      label: "1.306",
+      name: "5 Estrellas",
+    },
+    {
+      icon: <FiUsers />,
+      label: "35.000",
+      name: "Visitantes",
+    },
+    {
+      icon: <AiOutlineStar />,
+      label: "1.306",
+      name: "5 Estrellas",
+    },
+  ],
+  [ENGLISH]: [
+    {
+      icon: <FiUsers />,
+      label: "35.000",
+      name: "Visitors",
+    },
+    {
+      icon: <AiOutlineStar />,
+      label: "1.306",
+      name: "5 Stars",
+    },
+    {
+      icon: <FiUsers />,
+      label: "35.000",
+      name: "Visitors",
+    },
+    {
+      icon: <AiOutlineStar />,
+      label: "1.306",
+      name: "5 Stars",
+    },
+  ],
+};
+
+export const videoUrl =
+  "https://res.cloudinary.com/dxg9gszax/video/upload/v1643900779/kromac-ui/Avenged_Sevenfold_-_Wish_You_Were_Here_g2vaqu.mp4";
--- src/components/VideoPromo/VideoPromo.jsx
+++ src/components/VideoPromo/VideoPromo.jsx
@@ -1,11 +1,11 @@
 import React, { useRef, useState } from "react";
 import Grid from "../../layout/Grid";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
-import { cartagenaInfo } from "./data";
+import { cartagenaInfo, videoUrl } from "./VideoPromo.data";
+import { PAUSED } from "../../utils/constants";
 import "./VideoPromo.style.scss";
 
-const videoUrl =
-  "https://res.cloudinary.com/dxg9gszax/video/upload/v1643900779/kromac-ui/Avenged_Sevenfold_-_Wish_You_Were_Here_g2vaqu.mp4";
+const componentClassNamePrefix = "app__video__section";
 
 const VideoPromo = ({ languaje }) => {
   const [onPlayPause, setOnPlayPause] = useState(false);
@@ -19,22 +19,25 @@ const VideoPromo = ({ languaje }) => {
       videoRef.current.pause();
     }
   };
-  const isPaused = onPlayPause ? "paused" : "";
+  const isPaused = onPlayPause ? PAUSED : "";
 
   return (
-    <section className="app__video__section">
-      <section className="app__video__section__video" onClick={handlerPLayer}>
+    <section className={componentClassNamePrefix}>
+      <section
+        className={`${componentClassNamePrefix}__video`}
+        onClick={handlerPLayer}
+      >
         <video src={videoUrl} ref={videoRef} autoPlay={onPlayPause} muted />
         <button onClick={handlerPLayer} className={isPaused}>
           {onPlayPause ? <BsPauseFill /> : <BsPlayFill />}
         </button>
       </section>
-      <section className="app__video__section__info">
+      <section className={`${componentClassNamePrefix}__info`}>
         <Grid columns={4}>
           {cartagenaInfo[languaje].map((info, index) => (
             <section
               key={index}
-              className="app__video__section__info__icon__container"
+              className={`${componentClassNamePrefix}__info__icon__container`}
             >
               <section>
                 {info.icon}
