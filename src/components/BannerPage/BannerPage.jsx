d2602b29429b883e6702dee4f50cabade702e350
Narmatsu <joncyy_19@hotmail.com>
2024-03-05 16:05:10 -0500

    improving banner page component

--- src/components/BannerPage/BannerPage.jsx
+++ src/components/BannerPage/BannerPage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Container from "../../layout/Container";
 import "./BannerPage.style.scss";
 
+const componentClassNamePrefix = "banner__page";
+
 const BannerPage = ({ img, title }) => (
-  <section className="banner__page">
+  <section className={componentClassNamePrefix}>
     <img src={img} alt="banner" />
-    <Container width="70%" className="banner__page__container">
-      <section className="banner__page__caption">
+    <Container width="70%" className={`${componentClassNamePrefix}__container`}>
+      <section className={`${componentClassNamePrefix}__caption`}>
         <h3>{title}</h3>
       </section>
     </Container>
