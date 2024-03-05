c03e36d5886597d02bc655c69ee385f9b709b07d
Narmatsu <joncyy_19@hotmail.com>
2024-03-05 16:04:37 -0500

    improving cardclients testimonial component

--- src/components/CardClientsTestimonials/CardClientsTestimonials.jsx
+++ src/components/CardClientsTestimonials/CardClientsTestimonials.jsx
@@ -1,38 +1,22 @@
 import React from "react";
-import {
-  FaQuoteLeft,
-  FaQuoteRight,
-  FaRegStar,
-  FaStar,
-  FaStarHalfAlt,
-} from "react-icons/fa";
+import { builRateStars } from "../../utils";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 import "./CardClientsTestimonials.style.scss";
 
-const builRateStars = (rate) => {
-  const result = [];
-  const residue = rate - Math.trunc(rate);
-  for (let i = 1; i <= rate; i++) {
-    result.push(<FaStar />);
-  }
-  residue && result.push(<FaStarHalfAlt />);
-  for (let i = result.length; i < 5; i++) {
-    result.push(<FaRegStar />);
-  }
-  return result;
-};
+const componentClassNamePrefix = "card__clients__testimonials";
 
 const CardClientsTestimonials = ({ items, languaje, theme }) => (
-  <section className={`card__clients__testimonials ${theme}`}>
+  <section className={`${componentClassNamePrefix} ${theme}`}>
     <FaQuoteLeft className="icon__bg" />
     <FaQuoteRight className="icon__bg down" />
-    <section className="card__clients__testimonials__info">
+    <section className={`${componentClassNamePrefix}__info`}>
       <img src={items.img} alt="avatar" />
       <h3>{items.name}</h3>
     </section>
-    <section className="card__clients__testimonials__caption">
+    <section className={`${componentClassNamePrefix}__caption`}>
       <p>{items.testimonial[languaje]}</p>
     </section>
-    <section className="card__clients__testimonials__rate">
+    <section className={`${componentClassNamePrefix}__rate`}>
       {builRateStars(items.rate).map((star, index) => (
         <span key={index}>{star}</span>
       ))}
