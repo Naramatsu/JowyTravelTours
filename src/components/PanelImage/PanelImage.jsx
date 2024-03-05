2d48729ad4654fd84d917f9fda483e1e91f31c7b
Narmatsu <joncyy_19@hotmail.com>
2024-03-05 16:03:25 -0500

    improving Panel image component

--- src/components/PanelImage/PanelImage.jsx
+++ src/components/PanelImage/PanelImage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Grid from "../../layout/Grid";
+import { LEFT, RIGHT } from "../../utils/constants";
 import "./PanelImage.style.scss";
 
 const PanelImage = ({
@@ -8,19 +9,19 @@ const PanelImage = ({
   description,
   buttonText,
   theme = "",
-  side = "left",
+  side = LEFT,
   className = "",
   children,
 }) => (
   <Grid columns={2} className={`app__panel__image ${theme} ${className}`}>
-    {side === "left" && <img src={img} alt="banner" />}
+    {side === LEFT && <img src={img} alt="banner" />}
     <section className="app__panel__image__container">
       {title}
       {description}
       {buttonText && <button>{buttonText}</button>}
       {children}
     </section>
-    {side === "right" && <img src={img} className={side} alt="banner" />}
+    {side === RIGHT && <img src={img} className={side} alt="banner" />}
   </Grid>
 );
 
