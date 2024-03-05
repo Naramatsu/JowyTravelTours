6718a71a909a35a49fbdac2ce384bf9df9038596
Narmatsu <joncyy_19@hotmail.com>
2024-03-05 16:00:58 -0500

    fix undefined classname in Grid component

--- src/layout/Grid/Grid.jsx
+++ src/layout/Grid/Grid.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { columnsGenerator } from "../../utils";
 import "./Grid.style.scss";
 
-const Grid = ({ children, columns, className }) => {
+const Grid = ({ children, columns, className = "" }) => {
   const columnsTemplate = columnsGenerator(columns);
   return (
     <section
