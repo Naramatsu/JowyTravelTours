e86ecb6d57b2388752da97739bb035bf3b59ff19
Narmatsu <joncyy_19@hotmail.com>
2024-03-14 09:41:05 -0500

    added goback button in bannerpage

--- src/components/BannerPage/BannerPage.jsx
+++ src/components/BannerPage/BannerPage.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import Container from "../../layout/Container";
+import { FaAngleLeft } from "react-icons/fa6";
+import { useHistory } from "react-router-dom";
 import "./BannerPage.style.scss";
 
 const componentClassNamePrefix = "banner__page";
 
-const BannerPage = ({ img, title }) => (
-  <section className={componentClassNamePrefix}>
-    <img src={img} alt="banner" />
-    <Container width="70%" className={`${componentClassNamePrefix}__container`}>
-      <section className={`${componentClassNamePrefix}__caption`}>
-        <h3>{title}</h3>
-      </section>
-    </Container>
-  </section>
-);
+const BannerPage = ({ img, title }) => {
+  const history = useHistory();
+  return (
+    <section className={componentClassNamePrefix}>
+      <img src={img} alt="banner" />
+      <Container
+        width="70%"
+        className={`${componentClassNamePrefix}__container`}
+      >
+        <section className={`${componentClassNamePrefix}__caption`}>
+          <label className="goback" onClick={() => history.goBack()}>
+            <FaAngleLeft />
+            Go back
+          </label>
+          <h3>{title}</h3>
+        </section>
+      </Container>
+    </section>
+  );
+};
 
 export default BannerPage;
