cfeb1d1ec4f9719c4238cfc9d7a9ecc77793e76a
Narmatsu <joncyy_19@hotmail.com>
2024-04-29 09:58:12 -0500

    Question component styles was updated

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,27 +1,20 @@
-import React, { useState } from "react";
-import { ACTIVE } from "../../utils/constants";
+import React from "react";
 import { FaChevronDown } from "react-icons/fa6";
 import "./Question.style.scss";
 
-const Question = ({ index, question, description }) => {
-  const [isActive, setIsActive] = useState("");
-
-  const toggleActive = () => {
-    setIsActive(isActive ? "" : ACTIVE);
-  };
-
-  return (
-    <section className="frequentquestions__list__question">
-      <button className={`accordion ${isActive}`} onClick={toggleActive}>
-        <section className="question">
-          <label>{index}</label>
-          {question}
-        </section>
-        <FaChevronDown />
-      </button>
-      <section className="panel">{description}</section>
-    </section>
-  );
-};
-
+const Question = ({ index, question, description, isActive, onClick }) => (
+  <section className="frequentquestions__list__question">
+    <button
+      className={`accordion ${isActive}`}
+      onClick={() => onClick(index - 1)}
+    >
+      <section className="question">
+        <label>{index}</label>
+        {question}
+      </section>
+      <FaChevronDown />
+    </button>
+    <section className="panel">{description}</section>
+  </section>
+);
 export default Question;
